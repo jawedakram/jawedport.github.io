6f7e4dcf54dd10d79fb6382d0c5b5ffeca57ceb3
jawedakram <jawedakram0@gmail.com>
2023-08-05 05:28:28 -0700

    jawed_portfolio

---
+++ css/index.php
@@ -0,0 +1,66 @@
+<section class="education" id="education">
+<h2 class="journey-header">My <span>Journey</span></h2>
+<div class="education-row">
+    <div class="education-coloumn">
+        <h3 class="title">Education</h3>
+       
+        <div class="education-box">
+          <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i>2017-2018
+                        <h2>Master Degree-University</h2>
+                        <p>Lorem ipsum, dolor sit amet c</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="education-box">
+            <div class="education-content">
+                <div class="content">
+                    <div class="year"><i class='bx bxs-calendar'></i>2017-2018
+                        <h2>Master Degree-University</h2>
+                        <p>Lorem ipsum dolor .</p>
+
+                    </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="education-row">
+        <div class="education-coloumn">
+            <h3 class="title ">Experience</h3>
+           
+            <div class="education-box">
+              <div class="education-content">
+                    <div class="content">
+                        <div class="year"><i class='bx bxs-calendar'></i>2017-2018
+                            <h2>Master Degree-University</h2>
+                            <p>Lorem ipsum, dolor sit amet consectetur </p>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="education-box">
+                <div class="education-content">
+                    <div class="content">
+                        <div class="year"><i class='bx bxs-calendar'></i>2017-2018
+                            <h2>Master Degree-University</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, nisi.</p>
+    
+                        </div>
+                    </div>
+                </div>
+                </div>
+    
+            </div>
+        </div>
+
+    
+
+</div>
+</section>
